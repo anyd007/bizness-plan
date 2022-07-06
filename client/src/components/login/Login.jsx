6aaa5f7d547b10ce73a0278685d9afbc85f5d252
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-07-06 17:06:06 +0200

    create login component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+import { useNavigate } from "react-router-dom";
+import "./login.css"
 
 const Login = () =>{
 
+    const history = useNavigate()
     return(
         <div className="login">
             <div className="login__title">
+                <AiOutlineCloseSquare onClick={()=>history("/")} className="login__title--close"/>
                 <h2>LOGOWANIE</h2>
                 <p>podaj nazwę użytkownika i hasło stworzone w procesie rejestracji</p>
             </div>
-            
+            <div className="login__panel">
+                <div className="login__panel--username">
+                    <label htmlFor="log-username">
+                        <input type="text" name="log-username" id="log-username" 
+                             placeholder="NAZWA UŻYTKOWNIKA"/>
+                        <span className="spanAnime">NAZWA UŻYTKOWNIKA</span>
+                </label>
+                </div>
+                <div className="login__panel--password">
+                    <label htmlFor="log-password">
+                        <input type="text" className="log-password" id="log-password" placeholder="HASŁO" />
+                        <span className="spanAnime">HASŁO</span>
+                </label>
+                </div>
+                <div className="login__panel--button">
+                    <button>ZALOGUJ</button>
+                </div>
+            </div>
         </div>
     )
 }
